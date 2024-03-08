aa69003860ab736da98984e9927de8b94b0ae51e
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2024-03-09 07:49:18 +1300

    Begin session handling, handle auth errors via popup localstorage data

--- src/utility/Popup.ts
+++ src/utility/Popup.ts
@@ -6,12 +6,19 @@ export interface PopupDefinition {
 export default function popup (url: string, width = 600, height = 800) {
 	const left = (window.innerWidth - width) / 2 + window.screenLeft;
 	const top = (window.innerHeight - height) / 2 + window.screenTop;
-	return new Promise<void>(resolve => {
+	return new Promise<void>((resolve, reject) => {
+		localStorage.removeItem("Popup-Error-Code");
+		localStorage.removeItem("Popup-Error-Message");
 		const options = "width=" + width + ",height=" + height + ",top=" + top + ",left=" + left;
 		const oauthPopup = window.open(url, "Discord OAuth", options);
 		const interval = setInterval(() => {
 			if (oauthPopup?.closed) {
 				clearInterval(interval);
+				const code = +localStorage.getItem("Popup-Error-Code")!;
+				const message = localStorage.getItem("Popup-Error-Message");
+				if (code || message)
+					return reject(Object.assign(new Error(message ?? "Internal Server Error"), { code }));
+
 				resolve();
 			}
 		}, 100);
