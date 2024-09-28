9f31b5d8e9c2e2e719975c558a69473f08a80f53
Chiri Vulpes <chiri@chiri.works>
2024-09-28 15:44:21 +1200

    Depend on chirilang, stop using the old sass code

--- tasks/pre_watch_build.ts
+++ tasks/pre_watch_build.ts
@@ -1,11 +1,11 @@
-import env from "./env";
-import sass from "./sass";
-import _static from "./static";
-import ts from "./ts";
-import Task from "./utility/Task";
+import chiri from "./chiri"
+import env from "./env"
+import _static from "./static"
+import ts from "./ts"
+import Task from "./utility/Task"
 
 export default Task("pre_watch_build", task => task.series(
-	task.parallel(sass, _static),
+	task.parallel(chiri, _static),
 	_ => task.try(ts),
 	env,
-));
+))
--- tasks/watch.ts
+++ tasks/watch.ts
@@ -1,20 +1,18 @@
-import chokidar from "chokidar";
-import sass from "./sass";
-import _static from "./static";
-import { tsWatch } from "./ts";
-import Hash from "./utility/Hash";
-import Task from "./utility/Task";
+import chokidar from "chokidar"
+import chiriwatch from "./chiri"
+import _static from "./static"
+import { tsWatch } from "./ts"
+import Hash from "./utility/Hash"
+import Task from "./utility/Task"
 
 export default Task("watch", async task => {
-	chokidar.watch(["style/**/*.scss"], { ignoreInitial: true })
-		.on("all", () =>
-			task.debounce(sass));
+	await task.run(chiriwatch)
 
 	chokidar.watch(["static/**/*"], { ignoreInitial: true })
 		// eslint-disable-next-line @typescript-eslint/no-misused-promises
 		.on("all", async (event, path) => true
 			&& (await Hash.fileChanged(path))
-			&& task.debounce(_static, path));
+			&& task.debounce(_static, path))
 
-	await task.run(tsWatch);
-});
+	await task.run(tsWatch)
+})
