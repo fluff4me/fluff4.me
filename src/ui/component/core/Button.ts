c4d7a166a9750bdd5e0a483b1747e53dfa7f264a
Chiri Vulpes <chiri@chiri.works>
2024-10-20 14:52:23 +1300

    Make popover buttons flush

--- src/ui/component/core/Button.ts
+++ src/ui/component/core/Button.ts
@@ -1,7 +1,18 @@
+import type style from "style"
 import Component from "ui/Component"
+
+type ButtonType = keyof { [KEY in keyof typeof style as KEY extends `button-type-${infer TYPE}--${string}` ? TYPE
+	: KEY extends `button-type-${infer TYPE}` ? TYPE
+	: never]: string[] }
+
+interface ButtonTypeManipulator<HOST> {
+	(...buttonTypes: ButtonType[]): HOST
+	remove (...buttonTypes: ButtonType[]): HOST
+}
 
 interface ButtonExtensions {
 	readonly textWrapper: Component
+	type: ButtonTypeManipulator<this>
 	setDisabled (disabled: boolean, reason: string): this
 }
 
@@ -15,6 +26,20 @@ const Button = Component.Builder("button", (button): Button => {
 		.style("button")
 		.extend<ButtonExtensions>(button => ({
 			textWrapper: undefined!,
+			type: Object.assign(
+				(...types: ButtonType[]) => {
+					for (const type of types)
+						button.style(`button-type-${type}`)
+					return button
+				},
+				{
+					remove (...types: ButtonType[]) {
+						for (const type of types)
+							button.style.remove(`button-type-${type}`)
+						return button
+					},
+				},
+			),
 			setDisabled (disabled, reason) {
 				if (disabled)
 					disabledReasons.add(reason)
