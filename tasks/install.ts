12bc09face4ce1b81db761b81661d264e6a275ab
Chiri Vulpes <chiri@chiri.works>
2024-11-10 11:21:11 +1300

    Fix install task not working on build

--- tasks/install.ts
+++ tasks/install.ts
@@ -5,14 +5,14 @@ import Log from "./utility/Log"
 import Task from "./utility/Task"
 
 export default Task("install", async () => {
-	await Task.cli({ cwd: "src" }, "PATH:npm", "install")
+	await Task.cli({ cwd: "src" }, "PATH:npm", "ci")
 	if (Env.ENVIRONMENT === "dev") {
 		Log.info(`Installing ${ansi.lightCyan("api.fluff4.me@latest")}...`)
 		await Task.cli({ cwd: "src" }, "PATH:npm", "install", "api.fluff4.me@latest")
-
-		const typesPath = "src/node_modules/api.fluff4.me/index.d.ts"
-		const types = await fs.readFile(typesPath, "utf8")
-		await fs.writeFile(typesPath, types
-			.replace("interface ErrorResponse ", "interface ErrorResponse<T = any> "))
 	}
+
+	const typesPath = "src/node_modules/api.fluff4.me/index.d.ts"
+	const types = await fs.readFile(typesPath, "utf8")
+	await fs.writeFile(typesPath, types
+		.replace("interface ErrorResponse ", "interface ErrorResponse<T = any> "))
 })
