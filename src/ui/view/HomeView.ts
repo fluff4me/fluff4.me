057934415d54616c2e9c08771cbb36b5b2f0c121
Chiri Vulpes <chiri@chiri.works>
2024-11-18 10:55:28 +1300

    Only show home view test ui in dev

--- src/ui/view/HomeView.ts
+++ src/ui/view/HomeView.ts
@@ -6,6 +6,7 @@ import LabelledTable from "ui/component/core/LabelledTable"
 import TextEditor from "ui/component/core/TextEditor"
 import View from "ui/view/View"
 import ViewDefinition from "ui/view/ViewDefinition"
+import Env from "utility/Env"
 import MarkdownItHTML from "utility/string/MarkdownItHTML"
 
 export default ViewDefinition({
@@ -15,44 +16,46 @@ export default ViewDefinition({
 		const block = Block().appendTo(view)
 		const form = block.and(Form, block.title)
 
-		const output = Component("div")
-		Component("div")
-			.attributes.set("contenteditable", "plaintext-only")
-			.style.setProperty("white-space", "pre-wrap")
-			.style.setProperty("font", "inherit")
-			.style.setProperty("background", "#222")
-			.style.setProperty("width", "100%")
-			.style.setProperty("height", "400px")
-			.style.setProperty("padding", "0.5em")
-			.style.setProperty("box-sizing", "border-box")
-			.event.subscribe("input", event => {
-				const text = event.component.element.textContent ?? ""
-				const md = new MarkdownIt("commonmark", { html: true, breaks: true })
-				MarkdownItHTML.use(md, MarkdownItHTML.Options()
-					.disallowTags("img", "figure", "figcaption", "map", "area"))
-				console.log(md.parse(text, {}))
-				output.element.innerHTML = md.render(text)
-			})
-			.appendTo(form.content)
+		if (Env.isDev) {
+			const output = Component("div")
+			Component("div")
+				.attributes.set("contenteditable", "plaintext-only")
+				.style.setProperty("white-space", "pre-wrap")
+				.style.setProperty("font", "inherit")
+				.style.setProperty("background", "#222")
+				.style.setProperty("width", "100%")
+				.style.setProperty("height", "400px")
+				.style.setProperty("padding", "0.5em")
+				.style.setProperty("box-sizing", "border-box")
+				.event.subscribe("input", event => {
+					const text = event.component.element.textContent ?? ""
+					const md = new MarkdownIt("commonmark", { html: true, breaks: true })
+					MarkdownItHTML.use(md, MarkdownItHTML.Options()
+						.disallowTags("img", "figure", "figcaption", "map", "area"))
+					console.log(md.parse(text, {}))
+					output.element.innerHTML = md.render(text)
+				})
+				.appendTo(form.content)
 
-		output
-			.style.setProperty("font", "inherit")
-			.style.setProperty("background", "#222")
-			.style.setProperty("width", "100%")
-			.style.setProperty("padding", "0.5em")
-			.style.setProperty("margin-top", "1em")
-			.style.setProperty("box-sizing", "border-box")
-			.appendTo(form.content)
+			output
+				.style.setProperty("font", "inherit")
+				.style.setProperty("background", "#222")
+				.style.setProperty("width", "100%")
+				.style.setProperty("padding", "0.5em")
+				.style.setProperty("margin-top", "1em")
+				.style.setProperty("box-sizing", "border-box")
+				.appendTo(form.content)
 
-		const table = LabelledTable().appendTo(form.content)
+			const table = LabelledTable().appendTo(form.content)
 
-		table.label(label => label.text.set("test editor"))
-			.content((content, label, row) => {
-				const editor = TextEditor()
-					.setLabel(label)
-					.appendTo(content)
-				label.event.subscribe("click", () => editor.document?.focus())
-			})
+			table.label(label => label.text.set("test editor"))
+				.content((content, label, row) => {
+					const editor = TextEditor()
+						.setLabel(label)
+						.appendTo(content)
+					label.event.subscribe("click", () => editor.document?.focus())
+				})
+		}
 
 		return view
 	},
--- src/utility/Env.ts
+++ src/utility/Env.ts
@@ -1,20 +1,25 @@
 
 export interface IEnvironment {
-	ENVIRONMENT: "dev" | "beta" | "prod";
-	API_ORIGIN: string;
-	URL_ORIGIN: string;
-	BUILD_NUMBER?: string;
-	BUILD_SHA?: string;
+	ENVIRONMENT: "dev" | "beta" | "prod"
+	API_ORIGIN: string
+	URL_ORIGIN: string
+	BUILD_NUMBER?: string
+	BUILD_SHA?: string
 }
 
 interface Env extends Readonly<IEnvironment> { }
 class Env {
+
+	public get isDev () {
+		return this.ENVIRONMENT === "dev"
+	}
+
 	public async load () {
-		const origin = location.origin;
-		const root = location.pathname.startsWith("/beta/") ? "/beta/" : "/";
-		Object.assign(this, await fetch(origin + root + "env.json").then(response => response.json()));
-		document.documentElement.classList.add(`environment-${this.ENVIRONMENT}`);
+		const origin = location.origin
+		const root = location.pathname.startsWith("/beta/") ? "/beta/" : "/"
+		Object.assign(this, await fetch(origin + root + "env.json").then(response => response.json()))
+		document.documentElement.classList.add(`environment-${this.ENVIRONMENT}`)
 	}
 }
 
-export default new Env;
+export default new Env
