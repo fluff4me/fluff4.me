4e9c8cfc598e8c5e042a291fb1e8b4d9dbb51eab
Chiri Vulpes <chiri@chiri.works>
2024-12-07 20:40:24 +1300

    Disable form submit button when form is invalid

--- src/ui/component/core/Form.ts
+++ src/ui/component/core/Form.ts
@@ -2,6 +2,7 @@ import Component from "ui/Component"
 import ActionRow from "ui/component/core/ActionRow"
 import Block from "ui/component/core/Block"
 import Button from "ui/component/core/Button"
+import State from "utility/State"
 
 interface FormExtensions {
 	content: Component
@@ -16,6 +17,11 @@ const Form = Component.Builder((form, label: Component): Form => {
 		.style("form")
 		.ariaRole("form")
 		.ariaLabelledBy(label)
+
+	form.receiveDescendantInsertEvents()
+
+	const valid = State.Generator(() => (form.element as HTMLFormElement).checkValidity())
+	form.event.subscribe(["input", "change", "descendantInsert"], () => valid.refresh())
 
 	const content = (form.is(Block) ? form.content : Component())
 		.style("form-content")
@@ -32,6 +38,7 @@ const Form = Component.Builder((form, label: Component): Form => {
 		.extendJIT("submit", () => Button()
 			.type("primary")
 			.attributes.set("type", "submit")
+			.bindDisabled(valid.not, "invalid")
 			.appendTo(footer.right))
 })
 
