bb390570c3711c9f5a2733672ad163bf27b155d7
Chiri Vulpes <chiri@chiri.works>
2024-12-28 15:08:12 +1300

    Add .text.append and .text.prepend

--- src/ui/utility/TextManipulator.ts
+++ src/ui/utility/TextManipulator.ts
@@ -2,13 +2,27 @@ import type Component from "ui/Component"
 import StringApplicator from "ui/utility/StringApplicator"
 
 interface TextManipulator<HOST> extends StringApplicator.Optional<HOST> {
+	prepend (text: string): HOST
+	append (text: string): HOST
 }
 
 function TextManipulator (component: Component): TextManipulator<Component> {
-	return StringApplicator(component, value => {
-		component.element.textContent = value ?? null
-		return value
-	})
+	return Object.assign(
+		StringApplicator(component, value => {
+			component.element.textContent = value ?? null
+			return value
+		}),
+		{
+			prepend (text: string) {
+				component.element.prepend(document.createTextNode(text))
+				return component
+			},
+			append (text: string) {
+				component.element.append(document.createTextNode(text))
+				return component
+			},
+		}
+	)
 }
 
 export default TextManipulator
