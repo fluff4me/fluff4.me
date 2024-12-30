e332a7c4ad7b08a1a1b16a05520f778ce45804a0
Chiri Vulpes <chiri@chiri.works>
2024-12-30 13:16:53 +1300

    Display support link on author component

---
+++ src/ui/component/core/ExternalLink.ts
@@ -0,0 +1,18 @@
+import Component from "ui/Component"
+
+interface ExternalLinkExtensions {
+
+}
+
+interface ExternalLink extends Component, ExternalLinkExtensions { }
+
+const ExternalLink = Component.Builder("a", (component, href: string | undefined) => {
+	component.style("link", "link-external")
+
+	if (href !== undefined)
+		component.attributes.set("href", href)
+
+	return component
+})
+
+export default ExternalLink
