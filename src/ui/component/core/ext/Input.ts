cceccb5bf741efde3ac526849cbf316dca28f07a
Chiri Vulpes <chiri@chiri.works>
2025-01-09 21:52:50 +1300

    Add custom hint popover handling for tags editor
    - Fix \n not being handled the same way in QuiltHelper.renderWeave
    - Use QuiltHelper.renderWeave in `TextManipulator`
    - Add support for B, I, U, and S tags in language file
    - Pull progress wheel out of Input into its own ProgressWheel component
    - Add support for non-State objects passed to a lot of bind/use functions

--- src/ui/component/core/ext/Input.ts
+++ src/ui/component/core/ext/Input.ts
@@ -1,11 +1,14 @@
+import type { Weave } from 'lang/en-nz'
 import Component from 'ui/Component'
 import { BlockClasses } from 'ui/component/core/Block'
 import type Label from 'ui/component/core/Label'
 import type { PopoverInitialiser } from 'ui/component/core/Popover'
 import Popover from 'ui/component/core/Popover'
+import ProgressWheel from 'ui/component/core/ProgressWheel'
 import Slot from 'ui/component/core/Slot'
 import { AllowYOffscreen } from 'ui/utility/AnchorManipulator'
 import StringApplicator from 'ui/utility/StringApplicator'
+import type { StateOr } from 'utility/State'
 import State from 'utility/State'
 
 export interface InputExtensions {
@@ -13,6 +16,9 @@ export interface InputExtensions {
 	readonly hint: StringApplicator.Optional<this>
 	readonly maxLength: State<number | undefined>
 	readonly length: State<number | undefined>
+	getPopover (): Popover | undefined
+	/** Disable the default popover visibility handling — visible on input focus — in favour of custom handling */
+	setCustomPopoverVisibilityHandling (): this
 	setMaxLength (maxLength?: number): this
 	setRequired (required?: boolean): this
 	/**
@@ -20,91 +26,119 @@ export interface InputExtensions {
 	 * - Allows the label to use this component for information like whether it's required, etc
 	 */
 	setLabel (label?: Label): this
+	/**
+	 * Allows tweaking the hint popover for this input.
+	 * 
+	 * Controls the same initialiser as `setCustomHintPopover`, if that's also used.
+	 */
 	tweakPopover (initialiser: PopoverInitialiser<this>): this
+	/**
+	 * Permanently replace the default hint popover handling for this input with custom handling.
+	 * 
+	 * Controls the same initialiser as `tweakPopover`.
+	 */
+	setCustomHintPopover (initialiser: PopoverInitialiser<this>): this
 }
 
 interface Input extends Component, InputExtensions { }
 
-const Input = Component.Extension((component): Input => {
-	const hintText = State<string | undefined>(undefined)
-	const maxLength = State<number | undefined>(undefined)
-	const length = State<number | undefined>(undefined)
-	const unusedPercent = State.MapManual([length, maxLength], (length, maxLength) => length === undefined || !maxLength ? undefined : 1 - length / maxLength)
-	const unusedChars = State.MapManual([length, maxLength], (length, maxLength) => length === undefined || !maxLength ? undefined : maxLength - length)
-	const hasPopover = State.MapManual([hintText, maxLength], (hintText, maxLength) => !!hintText || !!maxLength)
-	let popover: Popover | undefined
-	hasPopover.subscribeManual(hasPopover => {
-		if (!hasPopover) {
-			popover?.remove()
-			popover = undefined
-			return
-		}
+function createHintText (hint: StateOr<string | Weave>) {
+	return Component()
+		.style('input-popover-hint-text')
+		.text.bind(hint)
+}
 
-		popover = Popover()
-			.anchor.from(component)
-			.anchor.add('off right', `.\\${BlockClasses.Main}`, 'aligned top', {
-				...AllowYOffscreen,
-				yValid (y, hostBox, popoverBox) {
-					// only align top if the popover box is taller than the host box
-					return popoverBox.height > (hostBox?.height ?? 0)
-				},
-			})
-			.anchor.add('off right', `.\\${BlockClasses.Main}`, 'centre', AllowYOffscreen)
-			.setNormalStacking()
-			.setCloseOnInput(false)
-			.style('input-popover')
-			.setOwner(component)
-			.tweak(popover => {
-				Slot.using(hintText, (slot, hintText) => !hintText ? undefined
-					: Component()
-						.style('input-popover-hint-text')
-						.text.set(hintText))
-					.appendTo(popover)
+const Input = Object.assign(
+	Component.Extension((component): Input => {
+		const hintText = State<string | undefined>(undefined)
+		const maxLength = State<number | undefined>(undefined)
+		const length = State<number | undefined>(undefined)
+		const popoverOverride = State(false)
+		const hasPopover = State.MapManual([hintText, maxLength, popoverOverride], (hintText, maxLength, override) => override || !!hintText || !!maxLength)
+		let popover: Popover | undefined
+		hasPopover.subscribeManual(hasPopover => {
+			if (!hasPopover) {
+				popover?.remove()
+				popover = undefined
+				return
+			}
 
-				Slot.using(maxLength, (slot, maxLength) => !maxLength ? undefined
-					: Component()
-						.style('input-popover-max-length')
-						.append(Component()
-							.style('input-popover-max-length-icon')
-							.style.bind(unusedPercent.mapManual(p => (p ?? 0) < 0), 'input-popover-max-length-icon--overflowing'))
-						.append(Component()
-							.style('input-popover-max-length-text')
-							.text.bind(unusedChars.mapManual(chars => chars === undefined ? '' : `${chars}`)))
-						.style.bindVariable('remaining', unusedPercent.mapManual(p => 1 - (p ?? 0))))
-					.appendTo(popover)
-			})
-			.tweak(popoverInitialiser, component)
-			.appendTo(document.body)
-	})
+			popover = Popover()
+				.anchor.from(component)
+				.anchor.add('off right', `.\\${BlockClasses.Main}`, 'aligned top', {
+					...AllowYOffscreen,
+					yValid (y, hostBox, popoverBox) {
+						// only align top if the popover box is taller than the host box
+						return popoverBox.height > (hostBox?.height ?? 0)
+					},
+				})
+				.anchor.add('off right', `.\\${BlockClasses.Main}`, 'centre', AllowYOffscreen)
+				.setNormalStacking()
+				.setCloseOnInput(false)
+				.style('input-popover')
+				.setOwner(component)
+				.tweak(popover => {
+					if (popoverOverride.value)
+						return
 
-	component.hasFocused.subscribeManual(hasFocused => popover?.toggle(hasFocused).anchor.apply())
+					Slot.using(hintText, (slot, hintText) => !hintText ? undefined : createHintText(hintText))
+						.appendTo(popover)
 
-	let popoverInitialiser: PopoverInitialiser<Component> | undefined
-	return component.extend<InputExtensions>(component => ({
-		required: State(false),
-		hint: StringApplicator(component, value => hintText.value = value),
-		maxLength,
-		length,
-		setMaxLength (newLength) {
-			maxLength.value = newLength
-			return component
-		},
-		setRequired: (required = true) => {
-			component.attributes.toggle(required, 'required')
-			component.required.value = required
-			return component
-		},
-		setLabel: label => {
-			component.setName(label?.for)
-			component.setId(label?.for)
-			label?.setInput(component)
-			return component
-		},
-		tweakPopover (initialiser) {
-			popoverInitialiser = initialiser as never
-			return component
-		},
-	}))
-})
+					ProgressWheel.Length(length, maxLength)
+						.appendTo(popover)
+				})
+				.tweak(popoverInitialiser, component)
+				.appendTo(document.body)
+		})
+
+		let customPopoverVisibilityHandling = false
+		component.hasFocused.subscribeManual(hasFocused => {
+			if (customPopoverVisibilityHandling)
+				return
+
+			popover?.toggle(hasFocused).anchor.apply()
+		})
+
+		let popoverInitialiser: PopoverInitialiser<Component> | undefined
+		return component.extend<InputExtensions>(component => ({
+			required: State(false),
+			hint: StringApplicator(component, value => hintText.value = value),
+			maxLength,
+			length,
+			setCustomPopoverVisibilityHandling () {
+				customPopoverVisibilityHandling = true
+				return component
+			},
+			getPopover: () => popover,
+			setMaxLength (newLength) {
+				maxLength.value = newLength
+				return component
+			},
+			setRequired: (required = true) => {
+				component.attributes.toggle(required, 'required')
+				component.required.value = required
+				return component
+			},
+			setLabel: label => {
+				component.setName(label?.for)
+				component.setId(label?.for)
+				label?.setInput(component)
+				return component
+			},
+			tweakPopover (initialiser) {
+				popoverInitialiser = initialiser as never
+				return component
+			},
+			setCustomHintPopover (initialiser) {
+				popoverInitialiser = initialiser as never
+				popoverOverride.value = true
+				return component
+			},
+		}))
+	}),
+	{
+		createHintText,
+	}
+)
 
 export default Input
