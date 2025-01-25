16d1851d7897757a0f327d1d32d9a18893ce787e
Chiri Vulpes <chiri@chiri.works>
2025-01-25 19:20:57 +1300

    Fix notifications popover not containing notifications until viewing full notifications view

--- src/utility/Type.ts
+++ src/utility/Type.ts
@@ -3,6 +3,7 @@ export type SupplierOr<T, ARGS extends any[] = []> = T | ((...args: ARGS) => T)
 export type PromiseOr<T> = T | Promise<T>
 export type AnyFunction<R = any> = (...args: any[]) => R
 export type Mutable<T> = { -readonly [P in keyof T]: T[P] }
+export type PartialMutable<T> = { -readonly [P in keyof T]?: T[P] }
 export type Nullish = null | undefined
 export type Falsy = false | '' | 0 | 0n | Nullish
 export type PartialRecord<K extends keyof any, T> = { [P in K]?: T }
