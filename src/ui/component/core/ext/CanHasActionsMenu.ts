70251652359dcd827743f31f89fa5b134ea84889
Chiri Vulpes <chiri@chiri.works>
2025-02-16 23:21:16 +1300

    Fix some content action positioning issues

--- src/ui/component/core/ext/CanHasActionsMenu.ts
+++ src/ui/component/core/ext/CanHasActionsMenu.ts
@@ -1,13 +1,15 @@
 import Component from 'ui/Component'
 import Button from 'ui/component/core/Button'
 import type ComponentInsertionTransaction from 'ui/component/core/ext/ComponentInsertionTransaction'
-import type { PopoverComponentRegisteredExtensions, PopoverInitialiser } from 'ui/component/core/Popover'
+import type { PopoverComponentRegisteredExtensions } from 'ui/component/core/Popover'
 import Popover from 'ui/component/core/Popover'
 import type { SlotInitialiserReturn } from 'ui/component/core/Slot'
 import Slot from 'ui/component/core/Slot'
 import Viewport from 'ui/utility/Viewport'
 import { mutable } from 'utility/Objects'
 import type State from 'utility/State'
+
+export type ReanchorTweaker<MENU extends ActionsMenuExtensions<string>> = (actionsMenu: MENU, isTablet: boolean) => unknown
 
 export interface ActionsMenuExtensions<ACTION_ID extends string> {
 	readonly actionsWrappers: Record<ACTION_ID, Slot>
@@ -17,6 +19,10 @@ export interface ActionsMenuExtensions<ACTION_ID extends string> {
 	prependAction<ID extends string, T> (id: ID, state: State<T>, initialiser: (slot: ComponentInsertionTransaction, value: T) => SlotInitialiserReturn): Omit<this, keyof ActionsMenuExtensions<string>> & ActionsMenuExtensions<ACTION_ID | ID>
 	insertAction<ID extends string> (id: ID, direction: 'before' | 'after', sibling: ACTION_ID, initialiser: (slot: Slot) => unknown): Omit<this, keyof ActionsMenuExtensions<string>> & ActionsMenuExtensions<ACTION_ID | ID>
 	insertAction<ID extends string, T> (id: ID, direction: 'before' | 'after', sibling: ACTION_ID, state: State<T>, initialiser: (slot: ComponentInsertionTransaction, value: T) => SlotInitialiserReturn): Omit<this, keyof ActionsMenuExtensions<string>> & ActionsMenuExtensions<ACTION_ID | ID>
+	/** Allow adding a tweaker function that will be called whenever the anchor positioning is reset, after the default positioning */
+	subscribeReanchor (tweaker: ReanchorTweaker<this>): this
+	/** Remove a registered anchor tweaker function */
+	unsubscribeReanchor (tweaker: ReanchorTweaker<this>): this
 }
 
 export interface ActionsMenu<ACTION_ID extends string> extends Popover, ActionsMenuExtensions<ACTION_ID> { }
@@ -25,15 +31,25 @@ const ActionsMenu = Component.Builder((component): ActionsMenu<never> => {
 	if (!component.is(Popover))
 		throw new Error('ActionsMenu must be a Popover')
 
+	const anchorTweakers = new Set<(actionsMenu: ActionsMenuExtensions<never>, isTablet: boolean) => unknown>()
 	const actionsWrappers: Record<string, Slot> = {}
 	return component.extend<ActionsMenuExtensions<never>>(component => ({
 		actionsWrappers,
+		anchorTweakers,
 		appendAction: makeActionInserter(wrapper =>
 			wrapper.appendTo(component)),
 		prependAction: makeActionInserter(wrapper =>
 			wrapper.prependTo(component)),
 		insertAction: makeActionInserter((wrapper, direction: 'before' | 'after', sibling: string) =>
 			wrapper.insertTo(component, direction, actionsWrappers[sibling])),
+		subscribeReanchor (tweaker) {
+			anchorTweakers.add(tweaker)
+			return component
+		},
+		unsubscribeReanchor (tweaker) {
+			anchorTweakers.delete(tweaker)
+			return component
+		},
 	}))
 
 	function makeActionInserter<PARAMS extends any[]> (insert: (actionWrapper: Slot, ...params: PARAMS) => unknown) {
@@ -63,6 +79,8 @@ type ActionsMenuInitialiser<ID extends string, T, RESULT> = (actionsMenu: Action
 export interface HasActionsMenuExtensions<ACTION_ID extends string = never> {
 	readonly actionsMenu: ActionsMenu<ACTION_ID>
 	tweakActions<MENU> (tweaker: ActionsMenuInitialiser<ACTION_ID, this, MENU>): this & HasActionsMenuExtensions<MENU extends ActionsMenuExtensions<infer ID> ? ID : never>
+	tweakActionsAnchor (tweaker: (actionsMenu: ActionsMenu<never>) => unknown): this
+	untweakActionsAnchor (tweaker: (actionsMenu: ActionsMenu<never>) => unknown): this
 }
 
 export interface CanHasActionsMenuExtensions {
@@ -72,7 +90,7 @@ export interface CanHasActionsMenuExtensions {
 
 interface CanHasActionsMenu extends Component, CanHasActionsMenuExtensions { }
 
-const CanHasActionsMenu = Component.Extension((component, popoverInitialiser?: PopoverInitialiser<Component>): CanHasActionsMenu => {
+const CanHasActionsMenu = Component.Extension((component, popoverInitialiser?: ActionsMenuInitialiser<never, Component, unknown>): CanHasActionsMenu => {
 	let hasActionsMenu = false
 	let actionsMenuButtonInserter: true | ((button: Button) => unknown) | undefined
 	return (component as Component & PopoverComponentRegisteredExtensions)
@@ -85,24 +103,29 @@ const CanHasActionsMenu = Component.Extension((component, popoverInitialiser?: P
 					addActionsMenuButton(component)
 
 				component.clearPopover().setPopover('hover', (popover, button) => {
-					mutable(component).actionsMenu = popover
+					const actionsMenu = mutable(component).actionsMenu = popover
 						.and(ActionsMenu)
 						.style('actions-menu-popover')
 						.append(Slot().style.remove('slot').style('actions-menu-popover-arrow'))
 						.tweak(popoverInitialiser, button)
 						.tweak(initialiser, button)
 
-					Viewport.tablet.use(popover, isTablet => {
+					Viewport.tablet.use(actionsMenu, isTablet => {
 						const tablet = isTablet()
-						popover.anchor.reset()
-						if (tablet) popover
+						actionsMenu.anchor.reset()
+						if (tablet) actionsMenu
 							.type.remove('flush')
 							.anchor.add('aligned right', 'off bottom')
 							.anchor.add('aligned right', 'off top')
-						else popover
+							.anchor.orElseHide()
+						else actionsMenu
 							.type('flush')
 							.anchor.add('off right', 'centre')
 							.anchor.orElseHide()
+
+						const { anchorTweakers } = actionsMenu as any as { anchorTweakers: Set<ReanchorTweaker<ActionsMenu<never>>> }
+						for (const tweaker of anchorTweakers)
+							tweaker(actionsMenu, tablet)
 					})
 				})
 
@@ -118,6 +141,14 @@ const CanHasActionsMenu = Component.Extension((component, popoverInitialiser?: P
 			tweakActions (tweaker) {
 				component.actionsMenu.tweak(tweaker, component)
 				return component as never
+			},
+			tweakActionsAnchor (tweaker) {
+				component.actionsMenu.subscribeReanchor(tweaker)
+				return component
+			},
+			untweakActionsAnchor (tweaker) {
+				component.actionsMenu.unsubscribeReanchor(tweaker)
+				return component
 			},
 		}))
 
