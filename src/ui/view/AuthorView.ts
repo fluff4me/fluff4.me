f11f730b59a53d8b7271992c961c4fed9efa8954
Chiri Vulpes <chiri@chiri.works>
2025-02-17 03:11:03 +1300

    Use paginator2 for author view

--- src/ui/view/AuthorView.ts
+++ src/ui/view/AuthorView.ts
@@ -1,11 +1,12 @@
 import EndpointAuthorGet from 'endpoint/author/EndpointAuthorGet'
 import EndpointWorkGetAllAuthor from 'endpoint/work/EndpointWorkGetAllAuthor'
+import PagedListData from 'model/PagedListData'
 import Session from 'model/Session'
 import Component from 'ui/Component'
 import Author from 'ui/component/Author'
 import Button from 'ui/component/core/Button'
 import Link from 'ui/component/core/Link'
-import Paginator from 'ui/component/core/Paginator'
+import Paginator2 from 'ui/component/core/Paginator2'
 import Slot from 'ui/component/core/Slot'
 import Work from 'ui/component/Work'
 import View from 'ui/view/shared/component/View'
@@ -24,7 +25,7 @@ export default ViewDefinition({
 		const author = response.data
 		return { author }
 	},
-	async create (params: AuthorViewParams, { author }) {
+	create (params: AuthorViewParams, { author }) {
 		const view = View('author')
 
 		Author(author)
@@ -32,7 +33,12 @@ export default ViewDefinition({
 			.setContainsHeading()
 			.appendTo(view.content)
 
-		const paginator = Paginator()
+		const works = PagedListData.fromEndpoint(25, EndpointWorkGetAllAuthor.prep({
+			params: {
+				author: params.vanity,
+			},
+		}))
+		Paginator2()
 			.viewTransition('author-view-works')
 			.tweak(p => p.title.text.use('view/author/works/title'))
 			.setActionsMenu(popover => popover
@@ -43,23 +49,18 @@ export default ViewDefinition({
 						.text.use('view/author/works/action/label/new')
 						.event.subscribe('click', () => navigate.toURL('/work/new'))))
 			)
+			.set(works, (slot, works) =>
+				slot.append(...works.map(workData =>
+					Link(`/work/${author.vanity}/${workData.vanity}`)
+						.and(Work, workData, author)
+						.viewTransition(false)
+						.type('flush')
+						.appendTo(slot))))
+			.orElse(slot => Component()
+				.style('placeholder')
+				.text.use('view/author/works/content/empty')
+				.appendTo(slot))
 			.appendTo(view.content)
-		const worksQuery = EndpointWorkGetAllAuthor.prep({
-			params: {
-				author: params.vanity,
-			},
-		})
-		await paginator.useEndpoint(worksQuery, (slot, works) =>
-			slot.append(...works.map(workData =>
-				Link(`/work/${author.vanity}/${workData.vanity}`)
-					.and(Work, workData, author)
-					.viewTransition(false)
-					.type('flush')
-					.appendTo(slot))))
-		paginator.orElse(slot => Component()
-			.style('placeholder')
-			.text.use('view/author/works/content/empty')
-			.appendTo(slot))
 
 		return view
 	},
