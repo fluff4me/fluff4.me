43310c1516341f5fcc33207b41b9489ad735527a
Chiri Vulpes <chiri@chiri.works>
2025-02-19 01:14:17 +1300

    Fix not using FeedGetAuthed on new view when logged in

--- src/ui/view/NewView.ts
+++ src/ui/view/NewView.ts
@@ -1,4 +1,7 @@
 import EndpointFeedGet from 'endpoint/feed/EndpointFeedGet'
+import EndpointFeedGetAuthed from 'endpoint/feed/EndpointFeedGetAuthed'
+import Session from 'model/Session'
+import Slot from 'ui/component/core/Slot'
 import WorkFeed from 'ui/component/WorkFeed'
 import View from 'ui/view/shared/component/View'
 import ViewDefinition from 'ui/view/shared/component/ViewDefinition'
@@ -10,9 +13,11 @@ export default ViewDefinition({
 		view.breadcrumbs.title.text.use('view/new/main/title')
 		view.breadcrumbs.description.text.use('view/new/main/description')
 
-		WorkFeed()
-			.viewTransition('new-view-feed')
-			.setFromEndpoint(EndpointFeedGet)
+		Slot()
+			.use(Session.Auth.loggedIn, loggedIn => WorkFeed()
+				.viewTransition('new-view-feed')
+				.setFromEndpoint(loggedIn ? EndpointFeedGetAuthed : EndpointFeedGet)
+			)
 			.appendTo(view.content)
 
 		return view
