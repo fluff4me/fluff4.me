4c6417e19e4c332372f51353dbd2ed5345e136ab
Chiri Vulpes <chiri@chiri.works>
2025-03-19 00:57:03 +1300

    Display aliases and relationships in TagBlock

--- src/ui/component/TagBlock.ts
+++ src/ui/component/TagBlock.ts
@@ -1,12 +1,17 @@
 import quilt from 'lang/en-nz'
 import Follows from 'model/Follows'
 import Session from 'model/Session'
+import type { TagId, TagsManifest } from 'model/Tags'
 import Tags from 'model/Tags'
 import Component from 'ui/Component'
 import Block from 'ui/component/core/Block'
 import Button from 'ui/component/core/Button'
+import Heading from 'ui/component/core/Heading'
+import Slot from 'ui/component/core/Slot'
 import type { TagData } from 'ui/component/Tag'
 import Tag from 'ui/component/Tag'
+import AbortPromise from 'utility/AbortPromise'
+import State from 'utility/State'
 
 interface TagBlockExtensions {
 
@@ -14,7 +19,11 @@ interface TagBlockExtensions {
 
 interface TagBlock extends Block, TagBlockExtensions { }
 
-const TagBlock = Component.Builder((component, tag: TagData): TagBlock => {
+const TagBlock = Component.Builder((component, tag: TagData, manifestIn?: TagsManifest): TagBlock => {
+	const manifest = State(manifestIn)
+	if (!manifest.value)
+		void Tags.getManifest().then(value => manifest.value = value)
+
 	const block = component
 		.and(Block)
 		.style('tag-block')
@@ -67,10 +76,50 @@ const TagBlock = Component.Builder((component, tag: TagData): TagBlock => {
 	tagComponent.categoryWrapper?.style('tag-block-tag-category')
 	tagComponent.nameWrapper.style('tag-block-tag-name')
 
+	block.content.style('tag-block-content')
+
 	Component()
 		.style('tag-block-description')
 		.setMarkdownContent(tag.description)
 		.appendTo(block.content)
+
+	if (tag.aliases?.length)
+		Component()
+			.style('tag-block-aliases')
+			.append(Heading()
+				.style('tag-block-section-heading')
+				.setAestheticStyle(false)
+				.text.use('tag/label/aliases'))
+			.append(...!tag.aliases ? [] : tag.aliases.map(Tag))
+			.appendTo(block.content)
+
+	const tagId: TagId = `${tag.category}: ${tag.name}`
+
+	const relationshipsTo = manifest.mapManual(manifest => manifest?.relationships[tagId] ?? [])
+	Slot().appendTo(block.content).use(relationshipsTo, AbortPromise.asyncFunction(async (signal, slot, relationships) => !relationships.length ? undefined
+		: Component()
+			.style('tag-block-relationships')
+			.append(Heading()
+				.style('tag-block-section-heading')
+				.setAestheticStyle(false)
+				.text.use('tag/label/relationships-to'))
+			.append(...(await Tags.resolve(relationships)).map(Tag))
+	))
+
+	const relationshipsFrom = manifest.mapManual(manifest => !manifest ? []
+		: Object.entries(manifest.relationships)
+			.filter(([from, to]) => to.includes(tagId))
+			.map(([from]) => from)
+	)
+	Slot().appendTo(block.content).use(relationshipsFrom, AbortPromise.asyncFunction(async (signal, slot, relationships) => !relationships.length ? undefined
+		: Component()
+			.style('tag-block-relationships')
+			.append(Heading()
+				.style('tag-block-section-heading')
+				.setAestheticStyle(false)
+				.text.use('tag/label/relationships-from'))
+			.append(...(await Tags.resolve(relationships)).map(Tag))
+	))
 
 	return block
 })
