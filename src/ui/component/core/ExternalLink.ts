7da710e278eaa4c94f15652e754cf00c1130a21d
Chiri Vulpes <chiri@chiri.works>
2025-04-19 22:24:24 +1200

    Protect users from accidentally going to someplace they don't want to be

--- src/ui/component/core/ExternalLink.ts
+++ src/ui/component/core/ExternalLink.ts
@@ -14,10 +14,24 @@ const ExternalLink = Component.Builder('a', (component, href: string | undefined
 	component
 		.and(HandlesMouseEvents)
 		.style('link', 'link-external')
-		.style.bind(component.attributes.get('href').mapManual(href => !!href), 'link-external--has')
+		.style.bind(component.attributes.get('href').truthy, 'link-external--has')
+
+	const text = Component()
+		.appendTo(component)
+
+	const isMaskedLink = text.text.state.mapManual(text => !!text && text !== href && /^https?:?\/\/?/.test(text))
+
+	component
+		.style.bind(isMaskedLink, 'link-external--is-masked')
+		.extendJIT('text', component => text.text.rehost(component))
 
 	if (href !== undefined)
 		component.attributes.set('href', href)
+
+	Component()
+		.style('link-external-real-domain', 'link-external--has')
+		.text.bind(component.attributes.get('href').mapManual(href => `[${href}]`))
+		.appendToWhen(isMaskedLink, component)
 
 	return component
 })
