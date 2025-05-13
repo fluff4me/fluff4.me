406f13912dc5fb93a74d9ac0c014d5d73c1708b0
Chiri Vulpes <chiri@chiri.works>
2025-05-14 09:17:40 +1200

    Add support for updating and deleting changelog items

--- src/ui/view/about/AboutViewRoadmap.ts
+++ src/ui/view/about/AboutViewRoadmap.ts
@@ -1,9 +1,13 @@
+import type { ChangelogInsertBody, ChangelogItem } from 'api.fluff4.me'
 import EndpointChangelogAdd from 'endpoint/changelog/EndpointChangelogAdd'
+import EndpointChangelogDelete from 'endpoint/changelog/EndpointChangelogDelete'
 import EndpointChangelogGet from 'endpoint/changelog/EndpointChangelogGet'
+import EndpointChangelogUpdate from 'endpoint/changelog/EndpointChangelogUpdate'
 import FormInputLengths from 'model/FormInputLengths'
 import Session from 'model/Session'
 import Component from 'ui/Component'
 import Block from 'ui/component/core/Block'
+import Button from 'ui/component/core/Button'
 import Form from 'ui/component/core/Form'
 import InfiniteScroll from 'ui/component/core/InfiniteScroll'
 import LabelledTable from 'ui/component/core/LabelledTable'
@@ -33,33 +37,43 @@ export default Component.Builder(component => {
 
 			block.content.style('view-type-about-changelog')
 
-			Block()
+			const editingItem = State<ChangelogItem | undefined>(undefined)
+
+			const bodyInput = TextEditor()
+				.setMaxLength(FormInputLengths.map(block, lengths => lengths?.changelog?.body))
+				.setRequired()
+
+			const timeInput = TextInput()
+				.setValidityHandler(input => {
+					if (!input.length.value)
+						return undefined
+
+					if (isNaN(new Date(input.value).getTime()))
+						return quilt => quilt['shared/form/invalid/time']()
+				})
+				.placeholder.use('shared/form/time/placeholder')
+
+			const versionInput = TextInput()
+				.filter((...segments) => (
+					segments.map(segment => segment.replace(/[^0-9]/g, ''))
+				) as [string, string, string])
+				.placeholder.set(`v${+Env.BUILD_NUMBER! || 0}`)
+
+			const editingBlock = Block()
 				.tweak(block => {
-					block.title.text.use('view/about/roadmap/create-changelog/title')
+					block.title.text.bind(editingItem.map(block, item => quilt => quilt[!item ? 'view/about/roadmap/changelog-editor/title/create' : 'view/about/roadmap/changelog-editor/title/update']()))
 					const form = block.and(Form, block.title) as Component<HTMLFormElement> & Block & Form
 
 					const table = LabelledTable().appendTo(form.content)
 
-					const bodyInput = TextEditor()
-						.setMaxLength(FormInputLengths.map(table, lengths => lengths?.changelog?.body))
-						.setRequired()
-					table.label(label => label.text.use('view/about/roadmap/create-changelog/label/body'))
+					table.label(label => label.text.use('view/about/roadmap/changelog-editor/label/body'))
 						.content((content, label) => content.append(bodyInput.setLabel(label)))
 
 					const currentTimeState = State.Generator(() => new Date().toLocaleString(navigator.language))
 					const i = setInterval(() => currentTimeState.refresh(), 1000)
 					block.onRemoveManual(() => clearInterval(i))
 
-					const timeInput = TextInput()
-						.setValidityHandler(input => {
-							if (!input.length.value)
-								return undefined
-
-							if (isNaN(new Date(input.value).getTime()))
-								return quilt => quilt['shared/form/invalid/time']()
-						})
-						.placeholder.use('shared/form/time/placeholder')
-					table.label(label => label.text.use('view/about/roadmap/create-changelog/label/time'))
+					table.label(label => label.text.use('view/about/roadmap/changelog-editor/label/time'))
 						.content((content, label) => content
 							.append(timeInput.setLabel(label))
 							.append(Paragraph().and(Placeholder).and(Small)
@@ -69,16 +83,12 @@ export default Component.Builder(component => {
 							)
 						)
 
-					const versionInput = TextInput()
-						.filter((...segments) => (
-							segments.map(segment => segment.replace(/[^0-9]/g, ''))
-						) as [string, string, string])
-						.placeholder.set(`v${+Env.BUILD_NUMBER! || 0}`)
-					table.label(label => label.text.use('view/about/roadmap/create-changelog/label/version'))
+					table.label(label => label.text.use('view/about/roadmap/changelog-editor/label/version'))
 						.content((content, label) => content.append(versionInput.setLabel(label)))
 
 					let submitting = false
-					form.submit.text.use('view/about/roadmap/create-changelog/action/submit')
+					form.submit
+						.text.bind(editingItem.map(form, item => quilt => quilt[!item ? 'view/about/roadmap/changelog-editor/action/submit/create' : 'view/about/roadmap/changelog-editor/action/submit/save']()))
 						.event.subscribe('click', async () => {
 							if (versionInput.value && isNaN(+versionInput.value))
 								return
@@ -88,13 +98,14 @@ export default Component.Builder(component => {
 
 							submitting = true
 
-							const response = await EndpointChangelogAdd.query({
-								body: {
-									body: bodyInput.useMarkdown(),
-									version: !versionInput.value ? +Env.BUILD_NUMBER! || 0 : +versionInput.value,
-									time: !timeInput.value ? undefined : new Date(timeInput.value).toISOString(),
-								},
-							})
+							const body: ChangelogInsertBody = {
+								body: bodyInput.useMarkdown(),
+								version: !versionInput.value ? +Env.BUILD_NUMBER! || 0 : +versionInput.value,
+								time: !timeInput.value ? undefined : new Date(timeInput.value).toISOString(),
+							}
+							const response = editingItem.value
+								? await EndpointChangelogUpdate.query({ params: { id: editingItem.value.id }, body })
+								: await EndpointChangelogAdd.query({ body })
 
 							submitting = false
 							if (toast.handleError(response))
@@ -105,17 +116,64 @@ export default Component.Builder(component => {
 							timeInput.value = ''
 							changelog.reset()
 						})
+
+					Button()
+						.text.use('view/about/roadmap/changelog-editor/action/cancel')
+						.event.subscribe('click', () => {
+							bodyInput.importMarkdown('')
+							versionInput.value = ''
+							timeInput.value = ''
+							editingItem.value = undefined
+						})
+						.appendToWhen(editingItem.truthy, block.footer.left)
 				})
 				.appendToWhen(Session.Auth.privileged.ChangelogModify, block.content)
 
 			changelog
-				.setFromEndpoint(EndpointChangelogGet.prep(), (changelog, slot, signal) => {
-					for (const item of changelog) {
+				.setFromEndpoint(EndpointChangelogGet.prep(), (changelogItems, slot, signal) => {
+					for (const item of changelogItems) {
 						Block()
 							.style('view-type-about-changelog-item')
-							.tweak(block => block.content.setMarkdownContent(item.body))
+							.tweak(block => block.content
+								.style('view-type-about-changelog-item-content')
+								.append(Component()
+									.style('view-type-about-changelog-item-body')
+									.setMarkdownContent(item.body)
+								)
+							)
 							.tweak(block => block.footer.left.and(Placeholder).and(Small).text.set(`v${item.version}`))
 							.tweak(block => block.footer.right.and(Placeholder).and(Small).text.set(new Date(item.time).toLocaleDateString(navigator.language)))
+							.setActionsMenu(popover => {
+								Button()
+									.type('flush')
+									.setIcon('pencil')
+									.text.use('view/about/roadmap/changelog-editor/action/edit')
+									.event.subscribe('click', () => {
+										editingItem.value = item
+										bodyInput.importMarkdown(item.body)
+										versionInput.value = `${item.version}`
+										timeInput.value = new Date(item.time).toISOString()
+										editingBlock.header.element.scrollIntoView({ behavior: 'smooth' })
+									})
+									.appendTo(popover)
+
+								Button()
+									.type('flush')
+									.setIcon('trash')
+									.text.use('view/about/roadmap/changelog-editor/action/delete')
+									.event.subscribe('click', async () => {
+										const response = await EndpointChangelogDelete.query({ params: { id: item.id } })
+										if (toast.handleError(response))
+											return
+
+										changelog.reset()
+									})
+									.appendTo(popover)
+							})
+							.tweak(block => {
+								block.primaryActions.style('view-type-about-changelog-item-primary-actions').appendTo(block.content)
+								block.header.remove()
+							})
 							.appendTo(slot)
 					}
 				})
