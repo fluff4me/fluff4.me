f9fc1ccc3568dc9d8eecadb0827475db7a508f44
Chiri Vulpes <chiri@chiri.works>
2025-06-01 22:06:12 +1200

    Maybe do it right this time :3

--- src/ui/component/FundraiserBar.ts
+++ src/ui/component/FundraiserBar.ts
@@ -8,11 +8,11 @@ export default Component.Builder((component, fundraiser: Fundraiser) => {
 
 	return component
 		.style('view-type-fundraiser-progress-bar')
-		.style.setVariable('progress', fundraiser.funds_raised / fundraiser.thresholds[0])
+		.style.setVariable('fundraiser-progress', Math.min(1, fundraiser.funds_raised / fundraiser.thresholds[0]))
 		.append(Component()
 			.style('view-type-fundraiser-progress-bar-background')
 			.append(Component().style('view-type-fundraiser-progress-bar-progress'))
 		)
-		.append(FundraiserAmount(fundraiser.funds_raised))
-		.append(FundraiserAmount(fundraiser.thresholds[0]))
+		.append(FundraiserAmount(Math.round(fundraiser.funds_raised / 100)))
+		.append(FundraiserAmount(Math.round(fundraiser.thresholds[0] / 100)))
 })
--- src/ui/utility/SiteStatusListener.ts
+++ src/ui/utility/SiteStatusListener.ts
@@ -1,4 +1,12 @@
-// import Store from 'utility/Store'
+import SiteStatus from 'model/SiteStatus'
+import Button from 'ui/component/core/Button'
+import Link from 'ui/component/core/Link'
+import Slot from 'ui/component/core/Slot'
+import FundraiserBar from 'ui/component/FundraiserBar'
+import PageListener from 'ui/utility/PageListener'
+import State from 'utility/State'
+import Store from 'utility/Store'
+import Time from 'utility/Time'
 
 interface SiteStatusListenerData {
 	dismissed: boolean
@@ -12,58 +20,58 @@ declare module 'utility/Store' {
 }
 
 namespace SiteStatusListener {
-	// const bannerState = State<boolean>(!(Store.items.status?.dismissed ?? false))
+	const bannerState = State<boolean>(!(Store.items.status?.dismissed ?? false))
 
 	export function listen () {
-		// setActive()
+		setActive()
 
-		// PageListener.visible.subscribeManual(setActive)
-		// navigate.state.subscribeManual(setActive)
+		PageListener.visible.subscribeManual(setActive)
+		navigate.state.subscribeManual(setActive)
 
-		// bannerState.useManual(async shown => {
-		// 	if (shown)
-		// 		await banner.queue(banner => {
-		// 			banner.body.style('view-type-fundraiser-banner')
-		// 			Slot().appendTo(banner.body).use(SiteStatus, (slot, status) => {
-		// 				const fundraiser = status?.fundraisers[0]
-		// 				if (!fundraiser)
-		// 					return
+		bannerState.useManual(async shown => {
+			if (shown)
+				await banner.queue(banner => {
+					banner.body.style('view-type-fundraiser-banner')
+					Slot().appendTo(banner.body).use(SiteStatus, (slot, status) => {
+						const fundraiser = status?.fundraisers[0]
+						if (!fundraiser)
+							return
 
-		// 				Link('/fundraiser')
-		// 					.text.use('fundraiser/title')
-		// 					.event.subscribe('Navigate', () => { banner.dismiss() })
-		// 					.appendTo(slot)
+						Link('/fundraiser')
+							.text.use('fundraiser/title')
+							.event.subscribe('Navigate', () => { banner.dismiss() })
+							.appendTo(slot)
 
-		// 				FundraiserBar(fundraiser)
-		// 					.appendTo(slot)
-		// 			})
+						FundraiserBar(fundraiser)
+							.appendTo(slot)
+					})
 
-		// 			Button()
-		// 				.style('view-type-fundraiser-banner-dismiss-button')
-		// 				.type('icon', 'flush')
-		// 				.setIcon('xmark')
-		// 				.event.subscribe('click', banner.dismiss)
-		// 				.appendTo(banner.body)
+					Button()
+						.style('view-type-fundraiser-banner-dismiss-button')
+						.type('icon', 'flush')
+						.setIcon('xmark')
+						.event.subscribe('click', banner.dismiss)
+						.appendTo(banner.body)
 
-		// 			banner.dismissed.matchManual(true, () => {
-		// 				Store.items.status = { dismissed: true, lastActive: Date.now() }
-		// 				bannerState.value = false
-		// 			})
-		// 		})
-		// })
+					banner.dismissed.matchManual(true, () => {
+						Store.items.status = { dismissed: true, lastActive: Date.now() }
+						bannerState.value = false
+					})
+				})
+		})
 	}
 
-	// function setActive () {
-	// 	const status = Store.items.status ?? { dismissed: false, lastActive: Date.now() }
+	function setActive () {
+		const status = Store.items.status ?? { dismissed: false, lastActive: Date.now() }
 
-	// 	if (status.dismissed && Date.now() - status.lastActive > Time.hours(2)) {
-	// 		status.dismissed = false
-	// 		bannerState.value = true
-	// 	}
+		if (status.dismissed && Date.now() - status.lastActive > Time.hours(2)) {
+			status.dismissed = false
+			bannerState.value = true
+		}
 
-	// 	status.lastActive = Date.now()
-	// 	Store.items.status = status
-	// }
+		status.lastActive = Date.now()
+		Store.items.status = status
+	}
 }
 
 export default SiteStatusListener
