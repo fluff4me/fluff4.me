f2b90b527dfe2ea9053937cc12f1b0e631c7409e
Bappity <12457072+bappitybup@users.noreply.github.com>
2025-06-04 23:53:01 +0100

    polyfill computedStyleMap on Element
    - Fallback to getComputedStyle().get() when computedStyleMap() is missing
    - Tested on Firefox, Firefox Developer Edition, Chrome and Edge
    - Only implements get() because that’s the only usage in the codebase atm as far as I know

--- src/utility/Elements.ts
+++ src/utility/Elements.ts
@@ -11,6 +11,19 @@ namespace Elements {
 		Define.set(Element.prototype, 'asType', function (this: Element, tagName): any {
 			return this.tagName.toLowerCase() === tagName ? this : undefined
 		})
+
+		if (!Element.prototype.computedStyleMap) { 
+			Define.set(Element.prototype, 'computedStyleMap', function (this: Element) {
+			const css = getComputedStyle(this)
+			return {
+				get (p: string) {
+					const v = css.getPropertyValue(p)
+					return v ? { toString: () => v } : undefined
+				},
+				// PUT OTHER METHODS DOWN HERE, only get() done for now because I can't find any uses of the other ones in the code
+			} as unknown as StylePropertyMapReadOnly
+			})
+		}
 	}
 }
 
