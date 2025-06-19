c7888e4ebbc553db8da981e1bf84321e3c815bc9
Chiri Vulpes <chiri@chiri.works>
2025-06-19 22:33:35 +1200

    Add support for visits

--- src/ui/component/Statistics.ts
+++ src/ui/component/Statistics.ts
@@ -4,7 +4,7 @@ import TextLabel from 'ui/component/core/TextLabel'
 import type { Quilt } from 'ui/utility/StringApplicator'
 
 interface StatisticsSectionExtensions {
-	stat (translation: Quilt.SimpleKey | Quilt.Handler, value?: number | Quilt.Handler): this
+	stat (translation: Quilt.SimpleKey | Quilt.Handler, value?: number | bigint | Quilt.Handler, tweak?: (label: TextLabel, value?: number | bigint | Quilt.Handler) => unknown): this
 }
 
 interface StatisticsSection extends Component, StatisticsSectionExtensions { }
@@ -17,13 +17,15 @@ const StatisticsSection = Component.Builder((component, title: Quilt.SimpleKey |
 			.text.use(title)
 		)
 		.extend<StatisticsSectionExtensions>(section => ({
-			stat (translation, value) {
+			stat (translation, value, tweak) {
 				if (value !== undefined)
 					TextLabel()
-						.tweak(label => label.label.text.use(translation))
-						.tweak(label => label.content.text.set(typeof value !== 'number' ? value
-							: value.toLocaleString(navigator.language)
+						.style('statistics-stat')
+						.tweak(label => label.label.style('statistics-stat-label').text.use(translation))
+						.tweak(label => label.content.style('statistics-stat-value').append(Component()
+							.text.set(typeof value !== 'number' && typeof value !== 'bigint' ? value : value.toLocaleString(navigator.language))
 						))
+						.tweak(tweak, value)
 						.appendTo(section)
 				return this
 			},
