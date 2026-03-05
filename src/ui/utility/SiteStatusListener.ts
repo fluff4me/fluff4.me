62e9a82b62a1c4ec2851e7ff6094dad5ab38b39f
Chiri Vulpes <chiri@chiri.works>
2026-03-05 13:57:17 +1300

    comment out the noisy fundraiser bar
    it'll be ok

--- src/ui/utility/SiteStatusListener.ts
+++ src/ui/utility/SiteStatusListener.ts
@@ -1,14 +1,4 @@
-import SiteStatus from 'model/SiteStatus'
-import Component from 'ui/Component'
-import Button from 'ui/component/core/Button'
-import Link from 'ui/component/core/Link'
-import Slot from 'ui/component/core/Slot'
-import FundraiserBar from 'ui/component/FundraiserBar'
 import PageListener from 'ui/utility/PageListener'
-import Env from 'utility/Env'
-import State from 'utility/State'
-import Store from 'utility/Store'
-import Time from 'utility/Time'
 
 interface SiteStatusListenerData {
 	dismissed: boolean
@@ -22,7 +12,7 @@ declare module 'utility/Store' {
 }
 
 namespace SiteStatusListener {
-	const bannerState = State<boolean>(!(Store.items.status?.dismissed ?? false))
+	// const bannerState = State<boolean>(!(Store.items.status?.dismissed ?? false))
 
 	export function listen () {
 		setActive()
@@ -30,60 +20,60 @@ namespace SiteStatusListener {
 		PageListener.visible.subscribeManual(setActive)
 		navigate.state.subscribeManual(setActive)
 
-		bannerState.useManual(async shown => {
-			if (!shown)
-				return
+		// bannerState.useManual(async shown => {
+		// 	if (!shown)
+		// 		return
 
-			if (Env.isDev)
-				return
+		// 	if (Env.isDev)
+		// 		return
 
-			await SiteStatus.getManifest()
-			await banner.queue(banner => {
-				banner.body.style('view-type-fundraiser-banner')
-				Slot().appendTo(banner.body).use(SiteStatus, (slot, status) => {
-					const fundraiser = status?.fundraisers[0]
-					if (!fundraiser)
-						return
+		// 	await SiteStatus.getManifest()
+		// 	await banner.queue(banner => {
+		// 		banner.body.style('view-type-fundraiser-banner')
+		// 		Slot().appendTo(banner.body).use(SiteStatus, (slot, status) => {
+		// 			const fundraiser = status?.fundraisers[0]
+		// 			if (!fundraiser)
+		// 				return
 
-					Link('/fundraiser')
-						.text.use('fundraiser/title')
-						.event.subscribe('Navigate', () => { banner.dismiss() })
-						.appendTo(slot)
+		// 			Link('/fundraiser')
+		// 				.text.use('fundraiser/title')
+		// 				.event.subscribe('Navigate', () => { banner.dismiss() })
+		// 				.appendTo(slot)
 
-					Component()
-						.style('view-type-fundraiser-progress-bar-wrapper')
-						.append(FundraiserBar(fundraiser.monthly_income, 100000, 'monthly'))
-						.append(FundraiserBar(fundraiser.funds_raised, fundraiser.thresholds[0], 'total')
-							.style('view-type-fundraiser-progress-bar--total')
-						)
-						.appendTo(slot)
-				})
+		// 			Component()
+		// 				.style('view-type-fundraiser-progress-bar-wrapper')
+		// 				.append(FundraiserBar(fundraiser.monthly_income, 100000, 'monthly'))
+		// 				.append(FundraiserBar(fundraiser.funds_raised, fundraiser.thresholds[0], 'total')
+		// 					.style('view-type-fundraiser-progress-bar--total')
+		// 				)
+		// 				.appendTo(slot)
+		// 		})
 
-				Button()
-					.style('view-type-fundraiser-banner-dismiss-button')
-					.type('icon', 'flush')
-					.setIcon('xmark')
-					.event.subscribe('click', banner.dismiss)
-					.appendTo(banner.body)
+		// 		Button()
+		// 			.style('view-type-fundraiser-banner-dismiss-button')
+		// 			.type('icon', 'flush')
+		// 			.setIcon('xmark')
+		// 			.event.subscribe('click', banner.dismiss)
+		// 			.appendTo(banner.body)
 
-				banner.dismissed.matchManual(true, () => {
-					Store.items.status = { dismissed: true, lastActive: Date.now() }
-					bannerState.value = false
-				})
-			})
-		})
+		// 		banner.dismissed.matchManual(true, () => {
+		// 			Store.items.status = { dismissed: true, lastActive: Date.now() }
+		// 			bannerState.value = false
+		// 		})
+		// 	})
+		// })
 	}
 
 	function setActive () {
-		const status = Store.items.status ?? { dismissed: false, lastActive: Date.now() }
+		// const status = Store.items.status ?? { dismissed: false, lastActive: Date.now() }
 
-		if (status.dismissed && Date.now() - status.lastActive > Time.hours(2)) {
-			status.dismissed = false
-			bannerState.value = true
-		}
+		// if (status.dismissed && Date.now() - status.lastActive > Time.hours(2)) {
+		// 	status.dismissed = false
+		// 	bannerState.value = true
+		// }
 
-		status.lastActive = Date.now()
-		Store.items.status = status
+		// status.lastActive = Date.now()
+		// Store.items.status = status
 	}
 }
 
